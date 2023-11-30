02d42fae8040c4ad043c5939025c4cc4f7583c62
ivanp <ivan.portyanko@yandex.ru>
2023-11-30 19:56:42 +0300

    feat: update build

--- build/static/js/22.dae1d296.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[22],{523:function(s,e,o){"use strict";o.r(e);o(0);var c=o(5);e.default=()=>Object(c.jsx)("div",{children:"Promo codes component works!"})}}]);
-//# sourceMappingURL=22.dae1d296.chunk.js.map
